a540e88ee9f6ac40b779a290f46ca82ad3e64dbf
HYnam <hiuyi.nam@uqconnect.edu.au>
2022-08-06 18:15:48 +0100

    user input date without validation

--- assignments/JsFunctionalProgramming/translink-parser.js
+++ assignments/JsFunctionalProgramming/translink-parser.js
@@ -1,3 +1,13 @@
 // Welcome message
 console.log("Welcome to the UQ Lakes station bus tracker!");
 
+// user input date
+const readline = require('readline').createInterface({
+    input: process.stdin, 
+    output: process.stdout
+});
+
+readline.question('What date will you depart UQ Lakes station by bus?', date => {
+    console.log(`you depart UQ Lakes station ${date}.`);
+    readline.close();
+});
