0ff1687318cab52ac872c99b6d92199e9c577d17
HYnam <hiuyi.nam@uqconnect.edu.au>
2022-08-15 12:14:22 +0100

    user input for both date and time done
    Without format checking

--- assignments/JsFunctionalProgramming/translink-parser.js
+++ assignments/JsFunctionalProgramming/translink-parser.js
@@ -2,12 +2,21 @@
 console.log("Welcome to the UQ Lakes station bus tracker!");
 
 // user input date
-const readline = require('readline').createInterface({
+const readline = require('readline')
+
+const depart = readline.createInterface({
     input: process.stdin, 
     output: process.stdout
 });
 
-readline.question('What date will you depart UQ Lakes station by bus?', date => {
-    console.log(`you depart UQ Lakes station ${date}.`);
-    readline.close();
+depart.question('What date will you depart UQ Lakes station by bus?', date => {
+    depart.question('What time will you depart UQ Lakes station by bus?', time => {
+        console.log(`you depart UQ Lakes station on ${date} at ${time}.`);
+        depart.close();
+    });
 });
+
+depart.on("close", function() {
+    process.exit(0);
+});
+
