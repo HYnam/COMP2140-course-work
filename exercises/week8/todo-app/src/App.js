a3e808cba51a6b661b88f6e9e201a216868acf7f
HYnam <hiuyi.nam@uqconnect.edu.au>
2022-09-13 11:55:22 +0100

    initial commit for to-do app
    new function will be added

---
+++ exercises/week8/todo-app/src/App.js
@@ -0,0 +1,70 @@
+import { useState } from "react";
+
+const initialTodos = [
+    { task: "Take out the trash", completed: false },
+    { task: "Walk the dog", completed: true },
+    { task: "Do the weekly quizzes", completed: false },
+];
+
+function App() {
+    
+    const [todos, setTodos] = useState(initialTodos);
+    
+    const toggleCompleted = (todo) => {
+        let result = todos.filter((_todo) => _todo.task !== todo.task);
+        setTodos([...result, { ...todo, completed: !todo.completed }]);
+    };
+    
+    const sortTodos = (todo, otherTodo) => {
+        if (todo.task < otherTodo.task) {
+            return -1;
+        }
+        if (todo.task > otherTodo.task) {
+            return 1;
+        }
+        return 0;
+    };
+    
+    return (
+        <>
+            <header>
+                <h1>Todo App</h1>
+            </header>
+            <hr />
+            <main>
+                <h2>List of Todos</h2>
+                {todos.sort(sortTodos).map((todo) => (
+                    <Todo
+                    key={todo.task}
+                    {...todo}
+                    toggleCompleted={() => toggleCompleted(todo)}
+                    />
+                ))}
+            </main>
+        </>
+    );
+
+}
+
+function Todo({ task, completed, toggleCompleted }) {
+    
+    const taskStyle = () => {
+        let result = { display: "inline-block" };
+        if (completed) {
+            result["text-decoration"] = "line-through";
+        }
+        return result;
+    };
+    
+    return (
+        <section>
+            <input type="checkbox" checked={completed} onChange={toggleCompleted} />
+            <p style={taskStyle()}>
+                {task}
+            </p>
+        </section>
+    );
+
+}
+
+export default App;
