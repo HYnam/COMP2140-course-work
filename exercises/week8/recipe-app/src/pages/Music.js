7dcb68e2a75850559cc200ed107ef2c7b61b92b2
HYnam <hiuyi.nam@uqconnect.edu.au>
2022-09-13 16:32:31 +0100

    Playing Synth Notes From Array

--- exercises/week8/recipe-app/src/pages/Music.js
+++ exercises/week8/recipe-app/src/pages/Music.js
@@ -8,17 +8,27 @@ export default function Music({ toneObject }) {
 
     function playNotesManually() {
         const now = toneObject.now();
+        
         synth.triggerAttackRelease("F#3", "8n", now);
         synth.triggerAttackRelease("D#3", "8n", now + 0.5);
         synth.triggerAttackRelease("C#3", "8n", now + 1);
     }
 
+    function playNotesFromArray() { 
+        const now = toneObject.now();
+        const sequence = ["C3", "D3", "E3", "F3", "G3", "A3", "B3", "C4"];
+        
+        sequence.forEach((note, time) => {
+            synth.triggerAttackRelease(note, "8n", now + (time / 4));   //Plays 0.25s apart
+        });
+    }
+
     return (
         <Template title="Music">
             <h3>Synth</h3>
-            <h4>Play One Note</h4>
+            <h4>Play Multiple Notes From Array</h4>
             <p>
-                <button onClick={playNotesManually}>Preview</button>
+                <button onClick={playNotesFromArray}>Preview</button>
             </p>
         </Template>
     );
