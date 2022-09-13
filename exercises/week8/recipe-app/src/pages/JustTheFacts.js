85c7045ac6d399e87789ece4f6dfc1daae8a79e1
HYnam <hiuyi.nam@uqconnect.edu.au>
2022-09-13 12:55:30 +0100

    Fetch API via States

--- exercises/week8/recipe-app/src/pages/JustTheFacts.js
+++ exercises/week8/recipe-app/src/pages/JustTheFacts.js
@@ -1,9 +1,29 @@
 import Template from "../components/Template";
+import { useState, useEffect} from 'react';
+
+function Fact({ fact }) {
+    return <p>{fact}</p>;
+}
 
 export default function JustTheFacts() {
+    const initialFact = "Loading fact...";
+    const [fact, setFact] = useState(initialFact);
+
+    useEffect(() => {
+        getFact();
+    }, []);
+    
+    async function getFact() {
+        const response = await fetch("https://catfact.ninja/fact");
+        const json = await response.json();
+        const fact = json.fact;
+        setFact(fact);
+    }
+
     return (
         <Template title="Just the Facts">
-            Just the Facts coming soon!
+            <h3>Random Fact</h3>
+            <Fact fact={fact} />
         </Template>
     );
 }
