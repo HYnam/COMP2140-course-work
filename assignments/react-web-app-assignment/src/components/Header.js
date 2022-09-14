6db1bde7e6c6a54d5acfd78b8d67c26345c3c2ac
HYnam <hiuyi.nam@uqconnect.edu.au>
2022-09-14 12:43:00 +0100

    Front page without styling

---
+++ assignments/react-web-app-assignment/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+    const navLinks = [
+        {
+            name: "Home",
+            url: "/",
+        },
+    ]
+}
---
+++ assignments/react-web-app-assignment/src/pages/App.js
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+import Template from '../components/Template';
+
+export default function App() {
+  return (
+    <Template title="Welcome to Share Music App!">
+        <p>More are coming</p>
+    </Template>
+  )
+}
