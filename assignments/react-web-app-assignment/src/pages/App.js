0c464e20fe3003002efa1c43745d5ebfa1617509
HYnam <hiuyi.nam@uqconnect.edu.au>
2022-09-17 17:00:09 +0100

    Read for samples and print out
    No samples created atm

--- assignments/react-web-app-assignment/src/pages/App.js
+++ assignments/react-web-app-assignment/src/pages/App.js
@@ -1,10 +1,37 @@
 import { Link } from "react-router-dom";
 import Template from '../components/Template';
+import { useState, useEffect } from "react";
+
+function SampleCard({ sample }) {
+  return (
+      <h4>{sample}</h4>
+  );
+}
 
 export default function App() {
+  // Read API for sample cards
+  const initialCard = "No music, please add one";
+  const [sample, setSample] = useState(initialCard);
+  
+  useEffect(() => {
+    getSampleCard();
+  }, []);
+
+  async function getSampleCard() {
+    const response = await fetch("http://wmp.interaction.courses/api/v1/?apiKey=2izT6jiZ&mode=read&endpoint=samples&limit=30&order=asc");
+    const json = await response.json();
+    const sample = json.sample;
+    setSample(sample);
+  }
+
   return (
-    <Template title="Welcome to Share Music App!">
-        <p>Create your own <Link to={`/music`}>music sharing list</Link>.</p>
+    <Template title="Samples You've Created">
+      <SampleCard sample={sample} />
+
+      <Link to="/create-sample">
+        <button>Create Sample</button>
+      </Link>
+      
     </Template>
   );
 }
