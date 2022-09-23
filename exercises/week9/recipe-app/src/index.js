b24ff0aa1419b75f9e670b7457a7406202d8de92
HYnam <hiuyi.nam@uqconnect.edu.au>
2022-09-23 18:52:11 +0100

    Guitar playing with preview button

---
+++ exercises/week9/recipe-app/src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./pages/App";
+import RecipeList from "./pages/RecipeList";
+import Recipe from "./pages/Recipe";
+import JustTheFacts from "./pages/JustTheFacts";
+import Music from "./pages/Music";
+import { toneObject, toneTransport, tonePart } from "./data/instruments.js";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/recipe" element={<RecipeList />} />
+                <Route path="/recipe/:id" element={<Recipe />} />
+                <Route path="/just-the-facts" element={<JustTheFacts />} />
+                <Route path="/music" element={<Music toneObject={toneObject} toneTransport={toneTransport} tonePart={tonePart} />} />
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
