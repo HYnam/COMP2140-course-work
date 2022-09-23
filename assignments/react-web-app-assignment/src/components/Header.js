68d373b9e7b894f5e86e256eae62562dd4244e37
HYnam <hiuyi.nam@uqconnect.edu.au>
2022-09-23 17:44:18 +0100

    Header & footer
    Button back to index page. Modify style later

--- assignments/react-web-app-assignment/src/components/Header.js
+++ assignments/react-web-app-assignment/src/components/Header.js
@@ -1,29 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Header() {
-    const navLinks = [
-        {
-            name: "Home",
-            url: "/",
-        },
-    ];
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
+export default function Header() {    
     return (
         <header className="page-header">
+            <Link to="/">
+                <button><FontAwesomeIcon icon={faArrowLeft} /></button>
+            </Link>
             <h1>OgCiSum</h1>
             <p>Create and Share Samples, Listen in Mobile App!</p>
-            <nav className="main-menu">
-                <ul>
-                    {navLinks.map((link) => {
-                        <Link key={link.url} to={link.url}>
-                            <li>
-                                {link.name}
-                            </li>
-                        </Link>
-                    })}
-                </ul>
-            </nav>
         </header>
     );
 
