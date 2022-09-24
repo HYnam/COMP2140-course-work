fbd7bbfc9e726c5e62bf6fbb1082920bd1fd0267
HYnam <hiuyi.nam@uqconnect.edu.au>
2022-09-24 10:41:19 +0100

    back arrow only appear in other pages
    Except index

--- assignments/react-web-app-assignment/src/components/Header.js
+++ assignments/react-web-app-assignment/src/components/Header.js
@@ -1,15 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
-export default function Header() {    
+export default function Header() { 
+    const location = useLocation()
+    
     return (
         <header className="page-header">
-            <Link to="/">
-                <button><FontAwesomeIcon icon={faArrowLeft} /></button>
-            </Link>
+            {location.pathname !=="/" && 
+                <Link to="/">
+                    <button><FontAwesomeIcon icon={faArrowLeft} /></button>
+                </Link>
+            }
+            
             <h1>OgCiSum</h1>
             <p>Create and Share Samples, Listen in Mobile App!</p>
         </header>
