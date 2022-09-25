d9edcb6d753adba38e2f1c173781c9c4b6d5ca3f
HYnam <hiuyi.nam@uqconnect.edu.au>
2022-09-25 15:56:01 +0100

    Note bar & styling

--- assignments/react-web-app-assignment/src/pages/CreateSameple.js
+++ assignments/react-web-app-assignment/src/pages/CreateSameple.js
@@ -1,11 +1,155 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Template from "../components/Template";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
-export default function CreateSample() {
+import Template from "../components/Template";
+import { guitar, piano, drums, frenchHorn } from "../data/instruments.js";
+
+function Bar ({ barID, barEnabled, handleBarClick }) {
+    function barSelected() {
+        if (barEnabled) {
+            return "selected";
+        }
+        return "";
+    }
+
     return (
-        <Template title = "Editing This Sample">
+        <div className={`bar bar-${barID} ${barSelected()}`} onClick={handleBarClick}>
+            {barID}
+        </div>
+    );
+}
+
+function Bars ({ sequence, setSequence, toneObject}) {
+    function sortSequence (bar, otherBar) {
+        if (bar.barID < otherBar.barID) {
+            return -1;
+        }
+        if (bar.barID > otherBar.barID) {
+            return 1;
+        }
+        return 0;
+    }
+
+    function handleBarClick(bar) {
+        const now = toneObject.now();
+        guitar.triggerAttackRelease("C3", "8n", now);
+        let filteredSequence = sequence.filter((_bar) => _bar.barID !== bar.barID);
+        setSequence([...filteredSequence, {...bar, barEnabled: !bar.barEnabled }]);
+    }
+    return sequence.sort(sortSequence).map(bar => <Bar key={bar.barID} {...bar} handleBarClick={() => handleBarClick(bar)} />);
+}
+
+function Preview ({ preview, setPreview, toneObject, toneTransport }) {
+    function handlePreviewClick() {
+        toneObject.start();
+        toneTransport.stop();
+        if (preview) {
+            setPreview(false);
+            console.log("Preview stopped manually");
+        } else {
+            setPreview(true);
+            console.log("Preview started...");
+            toneTransport.start();
+        }
+    }
+    return <button onClick={handlePreviewClick}>{preview ? "Stop Previewing" : "Preview"}</button>;
+}
+
+function Sequencer ({ toneObject, toneTransport, tonePart }) {
+    const initialSequence = [];
+    for (let bar = 1; bar <= 16; bar ++) {
+        initialSequence.push({
+            barID: bar, 
+            barEnabled: false,
+        });
+    }
+
+    const [sequence, setSequence] = useState(initialSequence);
+    const initialPreviewing = false;
+    const [preview, setPreview] = useState(initialPreviewing);
+
+    useEffect (() => {
+        tonePart.clear();
+        toneTransport.cancel();
+
+        sequence.filter(bar => bar.barEnabled).forEach(bar => {
+            tonePart.add((bar.barID - 1) / 4, "C3");
+        });
+
+        toneTransport.schedule(time => {
+            setPreview(false);
+            console.log("Preview stopped automatically");
+        }, 16/4);
+    });
+
+    return (
+        <>
+            <div className="sequencer">
+                <Bars sequence={sequence} setSequence={setSequence} toneObject={toneObject} />
+            </div>
+        </>
+    )
+}
+
+export default function CreateSample({ toneObject, toneTransport, tonePart }) {
+    let sampleName = <input type="text" name="samepleName" placeholder="Enter your music name" />
+
+    return (
+        <Template title = "Editing This Sample:">
+            <div className="card-detail">
+                <div className="card-name">
+                    <h2>{sampleName}</h2>
+                </div>
+
+                <button>Preview</button>
+                <button>Save</button>
+            </div>
+
+            <div className="instrument-select">
+                <div className="types"><p>Type</p></div>
+                <div className="instrument-button"><button>Piano</button></div>
+                <div className="instrument-button"><button>French Horn</button></div>
+                <div className="instrument-button"><button>Guitar</button></div>
+                <div className="instrument-button"><button>Drums</button></div>
+            </div>    
             
+
+            <div className="NoteB">
+                <div className="text"><p>B</p></div>
+                <div className="NoteChoice"><Sequencer toneObject={toneObject} toneTransport={toneTransport} tonePart={tonePart} /></div>
+            </div>
+
+            <div className="NoteA">
+                <div className="text"><p>A</p></div>
+                <div className="NoteChoice"><Sequencer toneObject={toneObject} toneTransport={toneTransport} tonePart={tonePart} /></div>
+            </div>
+
+            <div className="NoteG">
+                <div className="text"><p>G</p></div>
+                <div className="NoteChoice"><Sequencer toneObject={toneObject} toneTransport={toneTransport} tonePart={tonePart} /></div>
+            </div>
+            
+            <div className="NoteF">
+                <div className="text"><p>F</p></div>
+                <div className="NoteChoice"><Sequencer toneObject={toneObject} toneTransport={toneTransport} tonePart={tonePart} /></div>
+            </div>
+
+            <div className="NoteE">
+                <div className="text"><p>E</p></div>
+                <div className="NoteChoice"><Sequencer toneObject={toneObject} toneTransport={toneTransport} tonePart={tonePart} /></div>
+            </div>
+
+            <div className="NoteD">
+                <div className="text"><p>D</p></div>
+                <div className="NoteChoice"><Sequencer toneObject={toneObject} toneTransport={toneTransport} tonePart={tonePart} /></div>
+            </div>
+
+            <div className="NoteC">
+                <div className="text"><p>C</p></div>
+                <div className="NoteChoice"><Sequencer toneObject={toneObject} toneTransport={toneTransport} tonePart={tonePart} /></div>
+            </div>
+
         </Template>
     )
 }
