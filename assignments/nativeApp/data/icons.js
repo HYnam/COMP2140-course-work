eb19bf0e793f74c57b72f21627d579b32def02fe
HYnam <hiuyi.nam@uqconnect.edu.au>
2022-10-15 18:34:55 +0100

    images and libraries import

---
+++ assignments/nativeApp/data/icons.js
@@ -0,0 +1,23 @@
+// Bottom Tab Bar Icons
+import logo from "../App Logos Icons/app-launch.jpg";
+import logoDark from "../App Logos Icons/logo-darkpurple.png";
+import logoWhite from "../App Logos Icons/logo-white.png";
+
+import mapDark from "../App Logos Icons/tab-map-darkpurple.png";
+import mapWhite from "../App Logos Icons/tab-map-white.png";
+
+import profileDark from "../App Logos Icons/tab-profile-darkpurple.png";
+import profileWhite from "../App Logos Icons/tab-profile-white.png";
+
+// Music Nearby Page Icons
+import pinDark from "../App Logos Icons/icon-pin-darkpurple.png";
+import pinLight from "../App Logos Icons/icon-pin-lightpurple.png";
+import pinWhite from "../App Logos Icons/icon-pin-white.png";
+
+import smileDark from "../App Logos Icons/icon-smiley-darkpurple.png";
+import smileLight from "../App Logos Icons/icon-smiley-lightpurple.png";
+import smileWhite from "../App Logos Icons/icon-smiley-white.png";
+
+// Profile Page image
+import cat from "../App Logos Icons/cat.jpg";
+
