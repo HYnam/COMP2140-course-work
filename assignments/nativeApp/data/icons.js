8b099632073f2ef27b6cd47c702575512abb313f
HYnam <hiuyi.nam@uqconnect.edu.au>
2022-10-19 16:04:02 +0100

    photos files

--- assignments/nativeApp/data/icons.js
+++ assignments/nativeApp/data/icons.js
@@ -1,25 +1,25 @@
 // Bottom Tab Bar Icons
-import logo from "../App Logos Icons/app-launch.jpg";
-import logoDark from "../App Logos Icons/logo-darkpurple.png";
-import logoWhite from "../App Logos Icons/logo-white.png";
+import logo from "../assets/iconsImg/app-launch.jpg";
+import logoDark from "../assets/iconsImg/logo-darkpurple.png";
+import logoWhite from "../assets/iconsImg/logo-white.png";
 
-import mapDark from "../App Logos Icons/tab-map-darkpurple.png";
-import mapWhite from "../App Logos Icons/tab-map-white.png";
+import mapDark from "../assets/iconsImg/tab-map-darkpurple.png";
+import mapWhite from "../assets/iconsImg/tab-map-white.png";
 
-import profileDark from "../App Logos Icons/tab-profile-darkpurple.png";
-import profileWhite from "../App Logos Icons/tab-profile-white.png";
+import profileDark from "../assets/iconsImg/tab-profile-darkpurple.png";
+import profileWhite from "../assets/iconsImg/tab-profile-white.png";
 
 // Music Nearby Page Icons
-import pinDark from "../App Logos Icons/icon-pin-darkpurple.png";
-import pinLight from "../App Logos Icons/icon-pin-lightpurple.png";
-import pinWhite from "../App Logos Icons/icon-pin-white.png";
+import pinDark from "../assets/iconsImg/icon-pin-darkpurple.png";
+import pinLight from "../assets/iconsImg/icon-pin-lightpurple.png";
+import pinWhite from "../assets/iconsImg/icon-pin-white.png";
 
-import smileDark from "../App Logos Icons/icon-smiley-darkpurple.png";
-import smileLight from "../App Logos Icons/icon-smiley-lightpurple.png";
-import smileWhite from "../App Logos Icons/icon-smiley-white.png";
+import smileDark from "../assets/iconsImg/icon-smiley-darkpurple.png";
+import smileLight from "../assets/iconsImg/icon-smiley-lightpurple.png";
+import smileWhite from "../assets/iconsImg/icon-smiley-white.png";
 
 // Profile Page image
-import cat from "../App Logos Icons/cat.jpg";
+import cat from "../assets/iconsImg/cat.jpg";
 
 export default {
     logo,
